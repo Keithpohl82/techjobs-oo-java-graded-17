3154f2cf01d1619fa0fec8702be3614bbc8e40b7
keithpohl82 <keith.pohl82@gmail.com>
2024-09-25 18:39:49 -0500

    Removed comments

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -4,21 +4,6 @@ import java.util.Objects;
 
 public class JobField {
 
-    ///////////////// All have /////////////////
-
-    // private int id;
-    // private static int nextId = 1;
-    // private String value;
-    // toString method that returns value.
-    // A constructor that sets id = to nextid and nextid++
-    // A constructor to set value and init id constructor this().
-    // A Hashcode method.  - I am not sure what this method is doing
-    // getid method
-    // getvalue method
-    // setvalue method
-    // equals method - I am not sure what this method is doing
-
-    //////////////////////////////////////////////
 
     private int id;
     private static int nextId = 1;
@@ -43,7 +28,6 @@ public class JobField {
         return Objects.hashCode(id);
     }
 
-    // Getters and Setters:
 
     public int getId() {
         return id;
